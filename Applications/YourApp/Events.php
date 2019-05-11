ec3c58c65123af540858f349b3d7229b203d554f
vincent <phpvincent@163.com>
2019-05-11 17:59:28 +0800

    fix

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -20,7 +20,9 @@
 //declare(ticks=1);
 
 use \GatewayWorker\Lib\Gateway;
-
+//use \GatewayWorker\channel\getIpInfo\IpGet;
+require_once '..../vendor/mysql-master/src/Connection.php';
+require_once '/channel/getIpInfo/IpGet.php';
 /**
  * 主逻辑
  * 主要是处理 onConnect onMessage onClose 三个方法
@@ -28,6 +30,12 @@ use \GatewayWorker\Lib\Gateway;
  */
 class Events
 {
+    public static $db = null;
+    public static function onWorkerStart($worker)
+    {
+        //self::$db = new \Workerman\MySQL\Connection('172.31.37.203', '3306', 'admin', 'ydzsadmin', 'obj');
+        self::$db = new \Workerman\MySQL\Connection('127.0.0.1', '3306', 'root', '', 'obj');
+    }
     /**
      * 当客户端连接时触发
      * 如果业务不需此回调可以删除onConnect
@@ -36,10 +44,28 @@ class Events
      */
     public static function onConnect($client_id)
     {
+        $ip=$_SERVER['REMOTE_ADDR'];
+        Gateway::bindUid($client_id,$ip);
+
+        //得到地址信息
+        $IpGet=new IpGet($ip);
+        $ip_info=$IpGet->getIpMsg();
+        unset($IpGet);
+        $ip_info['country']=$ip_info->getCountry();
+        $time=date('Y-m-d H:i:S',time());
+
+        //记录全局信息
+        $_SESSION[$client_id]['ip_info']=$ip_info;
+        $_SESSION[$client_id]['first_time']=$time;
+
         // 向当前client_id发送数据 
-        Gateway::sendToClient($client_id, "Hello $client_id\r\n");
-        // 向所有人发送
-        Gateway::sendToAll("$client_id login\r\n");
+        static::msgToClient($client_id,['type'=>'connet_success','client_id'=>$client_id,'ip'=>$ip,'country'=>$ip_info['country'],'time'=>$time]);
+        
+        if($ip_info!=false && $ip_info!=null && $ip_info!=[]){
+          // 通知服务端
+          static::msgToAdmin($ip_info['country'],['type'=>'connet_notice','client_id'=>$client_id,'ip'=>$ip,'country'=>$ip_info['country'],'time'=>$time]);
+        }
+       
     }
     
    /**
@@ -50,7 +76,46 @@ class Events
    public static function onMessage($client_id, $message)
    {
         // 向所有人发送 
-        Gateway::sendToAll("$client_id said $message\r\n");
+        $msg=json_decode($message);
+        if(!isset($msg['user'])) GateWay::closeClient($client_id);
+        switch ($msg['user']) {
+          case 'client':
+            $ip_info=$_SESSION[$client_id]['ip_info'];
+            if(isset($ip_info['country'])&&$ip_info['country']!=null&&$ip_info['country']!='XX'){
+              Gateway::joinGroup($client_id, 'clent_'.$ip_info['country']);
+              static::msgToAdmin(1,$ip_info['country'],$msg);
+            }
+            break;
+          case 'admin':
+          //身份验证
+            if($msg['type']=='auth'){
+              $admin=self::$db->select('*')->from('admin')->where('admin_name='.$msg['admin_name'])->offset(0)->limit(1)->query();
+              if($admin==false||password_hash($admin['admin_password'])!=$admin['admin_password']){
+                static::msgToAdmin(0,$client_id,'auth unallow',-1);
+                return;
+              }else{
+                $_SESSION[$client_id]['auth']=$admin;
+                Gateway::joinGroup($client_id,$msg['language']);
+                static::msgToAdmin(0,$client_id,'auth success!');
+                return;
+              }
+            }
+          //数据推送
+            if(!isset($_SESSION[$client_id])||!isset($_SESSION[$client_id]['auth'])){
+              static::msgToAdmin(0,$client_id,'auth unallow',-1);
+              return;
+            }elseif($msg['touser']=='all'){
+              static::msgToClient('all',$msg['msg']);
+              return;
+            }else{
+              static::msgToClient($msg['touser'],$msg['msg']);
+            }
+            break;
+          default:
+            # code...
+            break;
+        }
+       //Gateway::sendToAll("$client_id said $message\r\n");
    }
    
    /**
@@ -60,6 +125,37 @@ class Events
    public static function onClose($client_id)
    {
        // 向所有人发送 
-       GateWay::sendToAll("$client_id logout\r\n");
+       GateWay::msgToAdmin("$client_id logout\r\n");
    }
+   /**
+    * 向客户端发送数据
+    * @param  [num] $client_id [id]
+    * @param  [num] $status [状态，0：成功]
+    * @param  [type] $msg    [数据]
+    * @return [type]         [description]
+    */
+   public static function msgToClient($client_id,$msg,$status=0)
+   {
+      if($client_id=='all'){
+        GateWay::sendToAll(json_decode(['touser'=>'client','status'=>$status,'msg'=>$msg]));
+      }
+     // 向当前client_id发送数据 
+      Gateway::sendToClient($client_id, json_encode(['touser'=>'client','status'=>$status,'msg'=>$msg]));
+   }
+   /**
+    * 向管理员发送数据
+    * @param  [type] $type [0:单独发送，1：群体发送]
+    * @param  [type] $status [description]
+    * @param  [type] $status [description]
+    * @param  [type] $msg    [description]
+    * @return [type]         [description]
+    */
+   public static function msgToAdmin($type=1,$group,$msg,$status=0)
+   {
+    if($type==1){
+     Gateway::sendToGroup($group,json_encode(['touser'=>'admin','status'=>$status,'msg'=>$msg]));
+    }else{
+     Gateway::sendToClient($group,json_encode(['touser'=>'admin','status'=>$status,'msg'=>$msg]));
+    }
+   }   
 }
---
+++ Applications/YourApp/channel/getIpInfo/IpGet.php
@@ -0,0 +1,104 @@
+<?php
+namespace GatewayWorker\channel\getIpInfo;
+use IpLocation;
+class IpGet {
+	private static $ipinfo;
+	private static $is_local=false;
+	public function __construct($ip)
+	{	
+		if($ip=='127.0.0.1') static::$is_local=true;
+		//$IpLocation=new IpLocation($ip);
+		static::$ipinfo=static::getIpInfo($ip);
+	}
+	public function getIpMsg()
+	{
+		return static::$ipinfo;
+	}
+	public function getCountry()
+	{
+		if(isset(static::$ipinfo['country'])&&static::$ipinfo['country']!='XX'&&static::$ipinfo['country']!=null){
+			$cou=static::$ipinfo['country'];
+		}else{
+			$cou=static::$ipinfo['city']=='XX' ? static::$ipinfo['region'] : static::$ipinfo['city'];
+		}
+		return $cou;
+	}
+	/**
+    * 根据Ip获取地址信息
+    * @param  [type]  $ip   [description]
+    * @param  boolean $type [description]
+    * @return [type]        [description]
+    */
+	private static function getIpInfo($ip)
+    { 
+      //set_time_limit(0);
+        $IpLocation=new IpLocation();
+        $ip = $IpLocation->getlocation($ip);
+        if($ip!=null&&$ip!=false&&$ip!=[]&&$ip!=''){
+          $iplo['ip']=$ip['ip'];
+          $iplo['country']=$ip['country'];
+          $iplo['city']=$ip['city'];
+          $iplo['region']=$ip['province'];
+          $iplo['county']=$ip['city'];
+          $iplo['area']=$ip['area'];
+          $iplo['isp']=$ip['area'];
+          if(strpos($iplo['isp'],"facebook")!==false||strpos($iplo['isp'],"Facebook")!==false||strpos($iplo['isp'],"脸书")!==false){
+             $iplo['isp']='脸书';
+          }
+          return $iplo;
+        }
+        //根据接口获取
+      if($ip!='127.0.0.1'){
+        //获取网络来源
+         $data = @file_get_contents('https://api.ip.sb/geoip/'.$ip);
+         $arr['isp']=json_decode($data,true)['organization'];
+      }else{
+         $data=true;
+         $arr['isp']='本机地址';
+      }     
+        //判断是否是脸书人员
+      if(strpos($arr['isp'],"facebook")!==false||strpos($arr['isp'],"Facebook")!==false){
+         $arr['isp']='脸书';
+      }
+      //获取地区信息
+      $area=@file_get_contents('https://freeapi.ipip.net/'.$ip);
+      $area=json_decode($area,true);
+      $arr['ip']=$request->getClientIp();
+      $arr['region']=isset($area[1])?$area[1]:'XX';
+      $arr['country']=isset($area[0])?$area[0]:'XX';
+      $arr['city']=isset($area[2])?$area[2]:'XX';
+      $arr['county']=isset($area[3])?$area[3]:'XX';
+      $arr['area']=isset($area[0])?$area[0]:'XX';
+     if($data==false||$area==false){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://ip.taobao.com/service/getIpInfo.php?ip=".$ip);
+        $wip=\App\vis::first()['vis_ip'];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-FORWARDED-FOR:8.8.8.8', "CLIENT-IP:".$wip));  //构造IP
+        curl_setopt($ch, CURLOPT_REFERER, "http://taobao.com.cn/");   //构造来路
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //curl_setopt($ch, CURLOPT_HEADER, 1);
+        $data = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if($httpCode == 502) {
+         $data = @file_get_contents('https://api.ip.sb/geoip/'.$ip);
+         $arr['country']=isset(json_decode($data,true)['country'])?json_decode($data,true)['country']:'XX';
+         $arr['area']=isset(json_decode($data,true)['continent_code'])?json_decode($data,true)['continent_code']:'XX';
+         $arr['region']=isset(json_decode($data,true)['region'])?json_decode($data,true)['region']:'XX';
+         $arr['city']=isset(json_decode($data,true)['city'])?json_decode($data,true)['city']:'XX';
+         $arr['county']=isset(json_decode($data,true)['county'])?json_decode($data,true)['county']:'XX';
+         $arr['isp']=isset(json_decode($data,true)['organization'])?json_decode($data,true)['organization']:'XX';
+         $arr['ip']=$request->getClientIp();
+         if(strpos($arr['isp'],"facebook")!==false||strpos($arr['isp'],"Facebook")!==false){
+           $arr['isp']='脸书';
+         }
+         return $arr;
+        }
+          curl_close($ch);
+          $arr=json_decode($data,true);
+          return $arr['data'];
+     }else{
+        return $arr;
+     }
+    return $arr;
+  }
+} 
